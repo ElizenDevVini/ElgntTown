cbedcf138d6ea62e913b7a8b601f3a45e59ab9de
Elizen <diamondkaz578@gmail.com>
2026-01-20 16:29:44 -0500

    Delete app directory

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-import dynamic from 'next/dynamic'
-
-// Client component (animations, intervals)
-const GargleExperiment = dynamic(() => import('../components/GargleExperiment'), { ssr: false })
-
-export default function Page() {
-  return <GargleExperiment />
-}
